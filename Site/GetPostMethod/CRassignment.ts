81026cdb33d239e33fe9befbb347278d95f3b5c9
JoanJeca <joannajeca936@gmail.com>
2023-09-20 09:44:58 +0100

    Fourth commit

--- Site/GetPostMethod/CRassignment.ts
+++ Site/GetPostMethod/CRassignment.ts
@@ -1,78 +1,80 @@
-import http,{ IncomingMessage, ServerResponse } from "http"
+    import { log } from 'console'
+import http, { IncomingMessage, ServerResponse } from 'http'
 
-interface iData{
-    id:number
-    name:string
-    stack:string
-    num:number
-}
-interface iMessage{
-    message:string
-    success:boolean
-    data: null | {} |{}[]
-    
-}
-
-const Data:iData[] =[
-    {
-        id:1,
-        name:"Peters",
-        stack:"Full-stack",
-        num:4759574982 
-    },
-    {
-    id:1,
-    name:"Samuel",
-    stack:"Full-stack",
-    num:474884646782 
-   },
-   {    id:1,
-    name:"Adisa",
-    stack:"Full-stack",
-    num:475489753 
-   },
-   {
-    id:4,
-    name:"Francis",
-    stack:"Full-stack",
-    num:4759574982 
-   },
-] 
-
-const port = 2005
-
-const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>)=>{
-    res.setHeader("Content-type", "application/json")
-    const {method, url} = req
-
-    let status = 404
-
-    let response:iMessage ={
-        message: "failed",
-        success: false,
-        data:null
+    interface iData{
+        id:number
+        name:string
+        age:number
+        stack:string
+    }
+    interface iResponse{
+        
+        message:string
+        success:boolean
+        data:null | {} |{}[]
     }
 
-    let container:any = []
+    const userInfo:iData[] =[
+        {
+            id:1,
+            name:"Joan",
+            age:78,
+            stack:"Front-end"
+        },
+        {
+            id:2,
+            name:"Chisom",
+            age:48,
+            stack:"Front-end"
+        },
+        {
+            id:3,
+            name:"Ada",
+            age:8,
+            stack:"Front-end"
+        },
+        {
+            id:1,
+            name:"Ewuzie",
+            age:78,
+            stack:"Front-end"
+        },
+    
+    ]
 
-    req
-       .on("data", (chunk:any)=>{
-        container.push(chunk)
-       })
-       .on("end", ()=>{
-        if(url === "/" && method === "GET"){
-            status= 200
-            response.message= "Successfully compiled"
-            response.success = true
-            response.data = Data
-            res.write(JSON.stringify({response, status}))
-            res.end()
+    const port = 2005
+    const server= http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>)=>{
+        res.setHeader("Content-type", "application/json")
+        const{method, url} = req
+
+        let status = 404
+
+        let response:iResponse ={
+            message:"Failed",
+            success:false,
+            data:null
         }
 
-        //post method
-        if(url ==="/" && method === "GET"){
-            const body = JSON.parse(container)
-            Data.push(body)
-        }
-       })
-})
+        const container:any = []
+
+        req
+           .on("data", (chunk:any)=>{
+              container.push(chunk)
+           })
+           .on("end", ()=>{
+              if(url === "/" && method==="GET"){
+                status = 200
+                response.message = "Successfull!"
+                response.success = true,
+                response.data = userInfo,
+                res.write(JSON.stringify({response, status})),
+                res.end()
+              }
+           })
+
+        
+    });
+
+    server.listen(port, ()=>{
+        console.log("Port running on", port)
+    })
